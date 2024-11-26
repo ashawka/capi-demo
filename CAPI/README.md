e05eccb016d54ff43e6638576f75b77054706c99
Mark Abrams <6570708+mak3r@users.noreply.github.com>
2024-11-26 11:42:46 -0500

    add capi autoscaling configuration and install script

---
+++ CAPI/README.md
@@ -0,0 +1,6 @@
+# Configure Autoscaling
+
+## Install the autoscaler in the CAPI management cluster
+
+1. `source CAPI/ready-autoscaler.sh`
+1. `install_autoscaler`
---
+++ CAPI/ready-autoscaler.sh
@@ -0,0 +1,12 @@
+#!/bin/sh
+
+# It would be nice to add some checks to see if the prerequisites are met
+# and also if the resources already exist in the management cluster
+
+
+install_autoscaler() {
+    export AUTOSCALER_NS=kube-system
+    export AUTOSCALER_IMAGE=registry.k8s.io/autoscaling/cluster-autoscaler:v1.29.0
+
+    envsubst < CAPI/ClusterAutoscaler.yaml | kubectl apply -f -
+}
